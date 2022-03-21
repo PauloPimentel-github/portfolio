1f9116dcefdc2c0133852c423f2b26f9cc570532
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-03-21 20:24:36 -0300

    First Commit :tada:

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
